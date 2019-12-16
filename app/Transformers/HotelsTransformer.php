de3e37278a7ddd1836e4375b346558920cb76647
Juan Antonio <juagisla@gmail.com>
2019-12-16 19:42:06 +0100

    Create ProfileTransformer

--- app/Transformers/HotelsTransformer.php
+++ app/Transformers/HotelsTransformer.php
@@ -2,21 +2,23 @@
 
 namespace App\Transformers;
 
-use App\Hotel;
-
 class HotelsTransformer extends Transformer {
 
     protected $resourceName = 'hotels';
 
+
     public function transform($data)
     {
+        $userTrans = new ProfileTransformer();
+        
         return [
             'id'        => $data['id'],
             'slug'      => $data['slug'],
             'stars'      => $data['stars'],
             'contry'  => $data['contry'],
             'company'  => $data['company'],
-            'hotelUsu' => UserTransformer::transforms($data['usuarios']),
+            'hotelUsu' => $userTrans->collection($data['usuarios'])
+            ,
             // 'hotelUsu' => $data['usuarios'] ->transform($data['usuarios']),
             // 'hotelUsu'  => [
             // 'id'      => $data['usuarios'][0]['id'],
